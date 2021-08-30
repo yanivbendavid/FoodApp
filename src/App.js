19dbcea56e46b6b44f501c2d610daba0a6db1292
Yaniv Ben David <yanivbd@gmail.com>
2021-08-30 14:38:59 +0300

    Added cart button (non functional)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import Header from "./components/Layout/Header";
+
 function App() {
   return (
     <div>
+     <Header/>
       <h2>Let's get started!</h2>
     </div>
   );
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Foodie</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="meals" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
