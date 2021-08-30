400872cbeaee12c8d7849bcaddbd291c4ca71875
Yaniv Ben David <yanivbd@gmail.com>
2021-08-30 18:25:41 +0300

    Added form input for meals (non functional)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-    <div>
-     <Header/>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
   );
 }
 
